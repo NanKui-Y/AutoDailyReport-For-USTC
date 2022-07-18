d6b81b0b17856cbd895940b1c9bc0e7907100f5f
NanKui-Y <86466744+NanKui-Y@users.noreply.github.com>
2022-07-18 17:18:41 +0800

    USTCC

--- runme.py
+++ runme.py
@@ -2,9 +2,9 @@ import argparse
 from Report import Report
 
 report_data = [
-    ("juzhudi", "中校区"),  # 居住地
-    ("dorm_building", "z2"),  # 宿舍楼
-    ("dorm", "414"),  # 宿舍号
+    ("juzhudi", "高新校区"),  # 居住地
+    ("dorm_building", "z1"),  # 宿舍楼
+    ("dorm", "709"),  # 宿舍号
     ("body_condition", "1"),  # 身体状况 1为正常
     ("body_condition_detail", ""),  # 身体状况详细 正常无需
     ("now_status", "1"),  # 当前状态 1为正常在校园内
@@ -15,9 +15,9 @@ report_data = [
     ("last_touch_sars_detail", ""),  # 具体情况
     ("is_danger", "0"),  # 当前居住地是否为疫情中高风险地区 0为否
     ("is_goto_danger", "0"),  # 14天内是否有疫情中高风险地区旅居史 0为否
-    ("jinji_lxr", "cyzkrau"),  # 紧急联系人
-    ("jinji_guanxi", "self"),  # 与本人关系
-    ("jiji_mobile", "18701375065"),  # 紧急联系人电话
+    ("jinji_lxr", "魏济洲"),  # 紧急联系人
+    ("jinji_guanxi", "boyfriend"),  # 与本人关系
+    ("jiji_mobile", "17719990726"),  # 紧急联系人电话
     ("other_detail", ""),  # 其他情况说明
 ]
 cross_campus_data = [
@@ -26,7 +26,7 @@ cross_campus_data = [
     ("return_college[]", "南校区"),  # 往返校区
     ("return_college[]", "北校区"),  # 往返校区
     ("return_college[]", "中校区"),  # 往返校区
-    ("reason", "上课"),  # 原因
+    ("reason", "a"),  # 原因
 ]
 out_school_data = [
     ("return_college[]", "蜀山区"),  # 目的地
